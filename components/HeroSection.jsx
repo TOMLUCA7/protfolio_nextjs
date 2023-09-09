41a48b2392940e80af826ae09e6a7c2ae71564c0
TOM LUCA <100872614+TOMLUCA7@users.noreply.github.com>
2023-09-09 12:53:04 +0300

    starting project portfolio and complete Hero Section component
    this component used Image , Tailwindcss for styling and react-type-animation library for animate the description

---
+++ components/HeroSection.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hello, I'm{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Tom Luca",
+                1500, // wait 1.5s before replacing "Tom Luca" with "Web Developer"
+                "Web Developer",
+                1500,
+                "Mobile Developer",
+                1500,
+                "Full Stack Developer",
+                1500,
+              ]}
+              wrapper="span"
+              speed={30}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptatum.
+          </p>
+          {/* buttons */}
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire me
+            </button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white border border-white mt-3">
+              Download CV
+            </button>
+          </div>
+        </div>
+        {/* image */}
+        <div className="col-span-5 place-self-center mt-4 mb-6 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
+            <Image
+              src="/images/tomImage.png"
+              alt="hero-image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
