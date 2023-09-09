7b6ba6ca846e4ab1dd4230108b5a15730a07e1c5
TOM LUCA <100872614+TOMLUCA7@users.noreply.github.com>
2023-09-09 15:28:35 +0300

    complete NavBar component
    complete navbar component added navigation links , and open menu logic on small Devices

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white">
               Hello, I'm{" "}
             </span>
             <br></br>
