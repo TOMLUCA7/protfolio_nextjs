54f4a84a492aa25c20d15331556f790be61054eb
TOM LUCA <100872614+TOMLUCA7@users.noreply.github.com>
2023-09-19 21:10:09 +0300

    Final commit

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-              Hello, I'm
+              Hello, I am
             </span>
             <br></br>
             <TypeAnimation
