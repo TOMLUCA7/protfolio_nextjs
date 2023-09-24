0f319142a1473a980cee0b41a1f39bf3f5898fa7
TOM LUCA <100872614+TOMLUCA7@users.noreply.github.com>
2023-09-24 18:55:58 +0300

    adding snow fall UI package

--- app/page.js
+++ app/page.js
@@ -1,21 +1,25 @@
+"use client";
+
 import HeroSection from "@/components/HeroSection";
 import NavBar from "@/components/NavBar";
 import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import EmailSection from "@/components/EmailSection";
 import Footer from "@/components/Footer";
+import Snowfall from "react-snowfall";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] h-full w-full p-3 items-center justify-center relative z-50">
+      <Snowfall />
       <NavBar />
       <div className="container mt-24 mx-auto py-4 px-12">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
         <EmailSection />
+        <Footer />
       </div>
-      <Footer />
     </main>
   );
 }
